25f4ef74669886253837a30cacc3c51cdb5bdf20
EPJrWorld <edwin.przezwiecki.jr@icloud.com>
2022-09-28 13:17:33 +0200

    Second challenge complete

--- Project17/GameScene.swift
+++ Project17/GameScene.swift
@@ -22,6 +22,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     let possibleEnemies = ["ball", "hammer", "tv"]
     var isGameOver = false
     var gameTimer: Timer?
+    var enemiesCreated = 0
+    var timeInterval = 1.0
     
     override func didMove(to view: SKView) {
         backgroundColor = .black
@@ -48,7 +50,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         physicsWorld.gravity = CGVector(dx: 0, dy: 0)
         physicsWorld.contactDelegate = self
         
-        gameTimer = Timer.scheduledTimer(timeInterval: 0.35, target: self, selector: #selector(createEnemy), userInfo: nil, repeats: true)
+        gameTimer = Timer.scheduledTimer(timeInterval: timeInterval, target: self, selector: #selector(createEnemy), userInfo: nil, repeats: true)
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -97,6 +99,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         let sprite = SKSpriteNode(imageNamed: enemy)
         sprite.position = CGPoint(x: 1200, y: Int.random(in: 50...736))
         addChild(sprite)
+        enemiesCreated += 1
         
         sprite.physicsBody = SKPhysicsBody(texture: sprite.texture!, size: sprite.size)
         sprite.physicsBody?.categoryBitMask = 1
@@ -104,6 +107,15 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         sprite.physicsBody?.angularVelocity = 5
         sprite.physicsBody?.linearDamping = 0
         sprite.physicsBody?.angularDamping = 0
+        
+        // Challenge 2:
+        if enemiesCreated >= 20 && enemiesCreated % 20 == 0 {
+            gameTimer?.invalidate()
+            if timeInterval > 0.1 {
+                timeInterval -= 0.1
+            }
+            gameTimer = Timer.scheduledTimer(timeInterval: timeInterval, target: self, selector: #selector(createEnemy), userInfo: nil, repeats: true)
+        }
     }
     // Challenge 1:
     func didBegin(_ contact: SKPhysicsContact) {
